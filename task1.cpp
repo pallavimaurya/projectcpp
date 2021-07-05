eb0b5db2c1ad3f3f427a9579e484c81d57afad0b
pallavimaurya <86961873+pallavimaurya@users.noreply.github.com>
2021-07-05 17:34:35 +0530

    created task1.cpp
    This file prints the size of int,float,double and char on screen.

---
+++ task1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ cout<<"size of int:"<<sizeof(int)<<"byte"<<endl;
+ cout<<"size of float:"<<sizeof(float)<<"byte"<<endl;
+ cout<<"size of double:"<<sizeof(double)<<"byte"<<endl;
+ cout<<"size of char:"<<sizeof(char)<<"byte"<<endl;
+ 
+ return 0;
+ }
